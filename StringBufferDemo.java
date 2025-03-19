5fcdad73298877d494486ac855cb2cb49aa2c8d8
Hemanth-kumar23 <hemantkumarr365@gmail.com>
2025-03-19 07:33:51 +0530

    update

--- StringBufferDemo.java
+++ StringBufferDemo.java
@@ -5,6 +5,15 @@ class StringBufferDemo {
         StringBuffer sb = new StringBuffer("Java");
 
         sb.append(" Programming");
-        System.out.println(sb);
+        System.out.println("After append: " + sb);
+
+        sb.insert(5, "Language ");
+        System.out.println("After insert: " + sb);
+
+        sb.replace(5, 13, "is a ");
+        System.out.println("After replace: " + sb);
+
+        sb.reverse();
+        System.out.println("After reverse: " + sb);
     }
 }
--- enumti.java
+++ enumti.java
@@ -7,7 +7,21 @@ class enumti {
 
     public static void main(String[] args) {
         Level l = Level.HIGH;
+        System.out.println("Selected Level: " + l);
 
-        System.out.println(l);
+        // Simple messages for each level
+        System.out.println("Is it LOW? " + (l == Level.LOW));
+        System.out.println("Is it MEDIUM? " + (l == Level.MEDIUM));
+        System.out.println("Is it HIGH? " + (l == Level.HIGH));
+
+        // Printing all levels
+        System.out.println("\nAvailable Levels:");
+        for (Level level : Level.values()) {
+            System.out.println(level);
+        }
+
+        // Changing level and printing again
+        l = Level.MEDIUM;
+        System.out.println("\nUpdated Level: " + l);
     }
 }
